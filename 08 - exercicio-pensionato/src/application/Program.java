490eef72ce10db4279a22087a939d7923a577d40
Daniel Menezes <dfm.daniel.menezes@gmail.com>
2026-06-09 19:08:27 -0300

    Add files via upload

---
+++ 08 - exercicio-pensionato/src/application/Program.java
@@ -0,0 +1,43 @@
+package application;
+
+import java.util.Scanner;
+
+import entities.Aluguel;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in); 
+		
+		Aluguel[] vect = new Aluguel[10];
+		
+		System.out.print("Quantos quartos vão ser alugados: ");
+		int n = sc.nextInt();
+		
+		for (int i=1; i<=n; i++) {
+			System.out.println();
+			System.out.println("Quarto #" + i + ":");
+			System.out.print("Nome: ");
+			sc.nextLine();
+			String nome = sc.nextLine();
+			System.out.print("Email: ");
+			String email = sc.next();
+			System.out.print("Quarto: ");
+			int quartoEscolhido = sc.nextInt();
+			
+			vect[quartoEscolhido] = new Aluguel(nome, email);
+		} 
+		
+		System.out.println();
+		System.out.println("Quartos ocupados: ");
+		for (int i=0; i<10; i++ ) {
+			if (vect [i] != null) { 
+				System.out.println("Quarto " + i + ":" + vect[i]);
+			}
+		}
+		
+		 
+		sc.close();
+	}
+}
